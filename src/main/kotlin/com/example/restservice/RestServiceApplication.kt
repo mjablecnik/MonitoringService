1b955be9ea42ab48cf8a10427162dc2a6dbc0353
Martin Jablecnik <martin.jablecnik@email.cz>
2020-11-24 20:29:22 +0100

    init

---
+++ src/main/kotlin/com/example/restservice/RestServiceApplication.kt
@@ -0,0 +1,13 @@
+package com.example.restservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class RestServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestServiceApplication>(*args)
+}
+
+
